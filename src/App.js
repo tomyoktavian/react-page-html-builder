a1a33616ba5f85e40e63f5356a3f2cb6582ac67d
hartomy <48695808+tomyoktavian@users.noreply.github.com>
2022-08-28 06:13:54 +0000

    setup page builder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import grapesjs from 'grapesjs';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      await grapesjs.init({
+        container: '#gjs',
+        height: '100vh',
+        width: 'auto',
+        storageManager: true,
+      });
+    })()
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="gjs"></div>
+      <div id="blocks"></div>
     </div>
   );
 }
